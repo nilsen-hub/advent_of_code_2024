b65144950c92f2160f52ea205787830d77bfef81
nilsen-hub <62958251+nilsen-hub@users.noreply.github.com>
2024-12-03 19:25:20 +0100

    day 3 stuff

--- day03_1/src/main.rs
+++ day03_1/src/main.rs
@@ -1,3 +1,49 @@
+use std::{fs::read_to_string, ops::Index, time::Instant};
+
 fn main() {
-    println!("Hello, world!");
+    let now = Instant::now();
+    let path = "./data/test";
+    let full_data = get_list_from_file(path);
+    babbage(full_data);
+    println!("program runtime: {}", now.elapsed().as_micros());
+}
+fn babbage(full_data: Vec<String>){
+    for line in full_data{
+        // Clone line, we will mangle it, and we will need it later.
+        let mut mul_strings: Vec<&str> = Vec::with_capacity(256);
+        let mut mul_locations: Vec<usize> = Vec::with_capacity(256);
+        let mut workbench = line.clone();
+        loop {
+            // first get a list of starting indices of "mul("
+            // will hopefully narrow down the search a bit
+            let mul = match workbench.find("mul(") {
+                Some(val) => val,
+                None => break,
+            };
+            let mut counter: usize = 10;
+            let mut string: &str = String::new();
+            'one: loop{
+                string = match workbench.get(mul..mul + counter) {
+                    Some(val) => {val; 
+                        break 'one;} 
+                    None => {counter -= 1; 
+                        continue;}
+                };
+            }
+            
+            // drain charavters up to and including mul, avoiding
+            // infinite loops
+            workbench.drain(0..mul + 1);
+            println!("{:?}", mul_locations);
+        }
+        for el in mul_locations
+    }
+    
+}
+fn get_list_from_file(path: &str) -> Vec<String> {
+    read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect()
 }
--- day03_1/target/debug/day03_1.d
+++ day03_1/target/debug/day03_1.d
@@ -1,1 +1,1 @@
-C:\Users\joach\Documents\code\rust\projects\advent_of_code_2024\day03_1\target\debug\day03_1.exe: C:\Users\joach\Documents\code\rust\projects\advent_of_code_2024\day03_1\src\main.rs
+D:\nilsen_temp\code\rust\advent_of_code_2024\day03_1\target\debug\day03_1.exe: D:\nilsen_temp\code\rust\advent_of_code_2024\day03_1\src\main.rs
