3f026e5478531d291cc00eede652d0abfde6f39c
nilsen-hub <62958251+nilsen-hub@users.noreply.github.com>
2024-12-10 20:33:41 +0100

    day 10 initialized

---
+++ day10_1/target/debug/day10_1.d
@@ -0,0 +1,1 @@
+D:\nilsen_temp\code\rust\advent_of_code_2024\day10_1\target\debug\day10_1.exe: D:\nilsen_temp\code\rust\advent_of_code_2024\day10_1\src\main.rs
---
+++ day10_1/target/debug/deps/day10_1-5a01bfd50448585b.d
@@ -0,0 +1,5 @@
+D:\nilsen_temp\code\rust\advent_of_code_2024\day10_1\target\debug\deps\libday10_1-5a01bfd50448585b.rmeta: src/main.rs
+
+D:\nilsen_temp\code\rust\advent_of_code_2024\day10_1\target\debug\deps\day10_1-5a01bfd50448585b.d: src/main.rs
+
+src/main.rs:
