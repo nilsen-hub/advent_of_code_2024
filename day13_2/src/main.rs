18d2174ab2bcb8989b14ef37d558c0fe038c860c
nilsen-hub <62958251+nilsen-hub@users.noreply.github.com>
2025-01-07 05:46:27 +0100

    final 13_2 optimisation
    Unless I make it recursive...

--- day13_2/src/main.rs
+++ day13_2/src/main.rs
@@ -1,91 +1,73 @@
 use std::{fs::read_to_string, time::Instant};
-type Coords = (i128, i128);
+type Coords = (usize, usize);
 
 #[derive(Debug, Clone)]
 struct ClawMachine {
     a: Coords,
     b: Coords,
     target: Coords,
-    //iterations: i128,
 }
 impl ClawMachine {
-    fn solve(&mut self) -> i128 {
+    fn solve(&mut self) -> usize {
 
         let target = self.target;
         let a = self.a;
         let b = self.b;
 
-        let a_increment = lcm(a.0, b.0) / a.0;
-        let mut a_presses: i128 = 0;
-        let mut position: Coords = (0, 0);
-
         // this removes machines that will never reach bx_mod == 0 + some more
         if target.0 % gcd(a.0, b.0) != 0 || target.1 % gcd(a.1, b.1) != 0{
             return 0;
         }
-        // this loop solves X
-        loop {
-            let bx_to_go = target.0 - position.0;
-            let bx_mod = bx_to_go % b.0;
 
+        let mut a_presses: usize = 0;
+        let mut position: Coords = (0, 0);
+        let mut x_to_go = usize::default();
+        
+        // this loop finds the first solution to X, defining an offset that
+        // allows us to use LCM to calculate the rest of the problem.
+        loop {
+            x_to_go = target.0 - position.0;
+            let bx_mod = x_to_go % b.0;
             if bx_mod == 0 {
                 break;
             }
             a_presses += 1;
-            position = (a.0 * a_presses, a.1 * a_presses);
-            // position tuple holds y value at current amount of a presses
-            // we use this information to calculate amount of b-presses 
-            // needed later.
+            position.0 = a.0 * a_presses;
         }
-        //self.iterations = a_presses;
-        // Now that we have our X, we ned to get Y into line
-        // first we have to find the value of Y at the moment to
-        // reference later:
-        let mut b_presses = (target.0 - position.0) / b.0; 
-        let first_y = (b.1 * b_presses) + position.1;      
+        
+        // Update position to hold needed data for next steps
+        position.1 = a.1 * a_presses;
 
-        // then we increment by one LCM derived step
-        // update our position and calculate the delta between
-        // the current Y value and our reference value.
-        // This gives us the value of each LCM increment
-        a_presses += a_increment;
-        position = (a.0 * a_presses, a.1 * a_presses);
-        b_presses = (target.0 - position.0) / b.0;
+        // Now that we have our X, we ned to get Y into line, we start
+        // by calculating how much Y increments pr. LCM derived increment
+        let lcm = lcm(a.0, b.0);
+        let a_increment = lcm / a.0;
+        let b_increment = lcm / b.0;
+        
+        let mut b_presses = x_to_go / b.0;
         let current_y = (b.1 * b_presses) + position.1;
-        let increment_y = first_y.abs_diff(current_y) as i128;
+        let increment_y = (a_increment*a.1).abs_diff(b_increment * b.1);
 
-        // Now that we have the increment value, we can see if it fits neatly
-        // into the delta between current y value and the target value.
+        // Now that we have the Y increment value, we can see if it fits neatly
+        // into the delta between the current y value and the target value.
         // If it fails, we can discard the Claw Machine. If sucsess, we know
         // the machine will solve
-        let delta_y_target = current_y.abs_diff(target.1) as i128;
+        let delta_y_target = current_y.abs_diff(target.1);
         if delta_y_target % increment_y != 0 {
-            //println!("Not good!");
-            //println!("");
             return 0;
         }
-        //println!("");
-        //println!("single a presses: {}", self.iterations);
-        //println!("a.0, b.0: {:?}", extended_gcd(a.0, b.0));
-        //println!("a.0, t.0: {:?}", extended_gcd(a.0, target.0));
-        //println!("b.0, t.0: {:?}", extended_gcd(b.0, target.0));
-        //println!("");
-        
-
-
         // To get the solution, we need to know how many a presses are needed
-        // to reach our target Y, luckily this is quite simple:
+        // to reach our target Y, first the a presses, luckily this is quite simple:
         let iter_to_go = delta_y_target / increment_y;
         a_presses += a_increment * iter_to_go;
 
         // then update the position to figure out how many b presses are needed
         // we could calculate b presses directly, but I think the added clarity
         // outwheighs the cost of the extra step
-        position = (a.0 * a_presses, a.1 * a_presses);
+        position.0 = a.0 * a_presses;
         b_presses = (target.0 - position.0) / b.0;
-        //println!("Good!");
-        //println!("");
-        // then we return
+        
+        // then we return the answer
         return (a_presses * 3) + b_presses;
     }
 }
@@ -114,7 +96,6 @@ impl InputData {
                 a: machine_coords[0],
                 b: machine_coords[1],
                 target: machine_coords[2],
-                //iterations: 0,
             });
             match input_iter.next() {
                 Some(_thing) => continue,
@@ -125,19 +106,18 @@ impl InputData {
     }
     fn get_target(&self, target: &str) -> Coords {
         let split: Vec<&str> = target.split('=').collect();
-        let x: i128 = split[1].strip_suffix(", Y").unwrap().parse().unwrap();
-        let y: i128 = split[2].parse().unwrap();
+        let x: usize = split[1].strip_suffix(", Y").unwrap().parse().unwrap();
+        let y: usize = split[2].parse().unwrap();
         (x + 10000000000000, y + 10000000000000)
     }
     fn get_button(&self, button: &str) -> Coords {
         let split: Vec<&str> = button.split('+').collect();
-        let x: i128 = split[1].strip_suffix(", Y").unwrap().parse().unwrap();
-        let y: i128 = split[2].parse().unwrap();
+        let x: usize = split[1].strip_suffix(", Y").unwrap().parse().unwrap();
+        let y: usize = split[2].parse().unwrap();
         (x, y)
     }
 }
 fn main() {
-    let now = Instant::now();
     let path = "./data/data";
     let full_data = match read_to_string(path) {
         Ok(data) => data,
@@ -145,58 +125,26 @@ fn main() {
     };
     let answer = babbage(full_data);
     println!("The answer is: {}", answer);
-    println!("program runtime: {}", now.elapsed().as_micros());
 }
-fn babbage(input: String) -> i128 {
+fn babbage(input: String) -> usize {
+    let now = Instant::now();
     let mut acc = 0;
     let input = InputData { input };
     let machines = input.get_machines();
-    //let mut iter_acc = 0;
-    //let mut greatest_iter = 0;
     for mut machine in machines {
         acc += machine.solve();
-        //iter_acc += machine.iterations;
-        //if machine.iterations > greatest_iter{
-        //    greatest_iter = machine.iterations;
-        //}
     }
-    //println!("total iterations: {} greatest amount needed: {}", iter_acc, greatest_iter);
+    println!("babbage runtime: {}", now.elapsed().as_micros());
     acc
 }
-#[derive(Debug, Clone)]
-struct ExtendedGCD{
-    part_1: (i128, i128),
-    part_2: i128,
-    part_3: (i128, i128),
-}
-fn extended_gcd(a: i128, b: i128) -> ExtendedGCD {
-    let (mut old_r, mut r) = (a, b);
-    let (mut old_s, mut s) = (1, 0);
-    let (mut old_t, mut t) = (0, 1);
-    
-    while r != 0 {
-        let quotient = old_r / r;
-        (old_r, r) = (r, old_r - (quotient * r));
-        (old_s, s) = (s, old_s - (quotient * s));
-        (old_t, t) = (t, old_t - (quotient * t));
-    }
-
-    let output = ExtendedGCD{
-        part_1: (old_s, old_t),
-        part_2: old_r,
-        part_3: (t, s),
-    };
-    output
-}
-
-fn gcd(a: i128, b: i128) -> i128 {
+fn gcd(a: usize, b: usize) -> usize {
     if b == 0 {
         return a;
     } else {
         return gcd(b, a % b);
     }
 }
-fn lcm(a: i128, b: i128) -> i128 {
+fn lcm(a: usize, b: usize) -> usize {
     return a * (b / gcd(a, b));
 }
 #[cfg(test)]
